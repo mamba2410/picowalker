97479cfc05f426be9ca42117dbcc020fec4a1a50
Mamba2410 <callummcgregor54@gmail.com>
2022-11-06 17:18:07 +0000

    eeprom: proper ram reads

--- src/drivers/eeprom_pico_ram.c
+++ src/drivers/eeprom_pico_ram.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "eeprom_pico_ram.h"
 
@@ -11,9 +12,9 @@ void pw_eeprom_raw_init() {
 }
 
 void pw_eeprom_raw_read(uint16_t addr, uint8_t *buf, size_t len) {
-
+    memcpy(buf, &(pw_ram_eeprom[addr]), len);
 }
 
 void pw_eeprom_raw_write(uint16_t addr, uint8_t *buf, size_t len) {
-
+    memcpy(&(pw_ram_eeprom[addr]), buf, len);
 }
