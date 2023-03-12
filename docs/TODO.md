24182a9b649526cb4e875c05c21d590577b2693c
Mamba2410 <callummcgregor54@gmail.com>
2023-03-12 19:54:24 +0000

    types: update types file

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,28 +1,40 @@
 # TODO
 
+See focalboard.
+
+
 ## Frontburner
 
-- Refactor IR code
-- Merge IR code from `pw-ir-linux`
 - Sort and add a key to the eeeprom map.
     - Make names consistent.
     - Identify what things are route-specific and what are inventory
-- Add IR error messages
 - Add `substate_request_redraw` which calls `state_draw_init()` again
+- Add ram cache for health data.
 
 
 ## Backburner
 
+- Implement more IR slave command responses
+- IR messages when comms end
+- Walk start/end animations
 - General refactor/clean up
-- Fix pico eeprom driver
 - Speed up screen I2C speed?
-- Minigames
+- Poke Radar
 - Accelerometer
 - Sound
 - Watchdog reset
 - Screensaver after time limit
 - Change buttons to general input?
 - Change button code to polling instead of interrupts?
+- add copying eeprom from existing walker
+
+
+## Bugs
+
+- Inventory
+    - Do not open if no inventory
+    - Cursor bugs on going backwards
+    - do not open page 2 if no gifted items
 
 
 ## Apps
@@ -30,17 +42,9 @@
 - PokeRadar
     - Countdown timer/alarm
 - Dowsing
-    - Read route items
-    - Write to inventory
-    - Choose item override if full
-    - Choose bush
-    - RNG
     - Read min item steps?
     - Read current steps
-- Connect
-    - IR handshake
-    - Master peer play
-    - Slave actions
+    - Determine item based on current steps and percentages
 - Settings
     - Sound
     - Shade
--- src/types.h
+++ src/types.h
@@ -52,35 +52,43 @@ typedef struct {
 } event_bitmap_t;
 
 
+/*
+ *  size: 0x68 = 104 bytes
+ *  Dmitry: struct IdentityData
+ */
 typedef struct {
-    uint32_t le_unk0;
-    uint32_t le_unk1;
-    uint16_t le_unk2;
-    uint16_t le_unk3;
-    uint16_t le_tid;
-    uint16_t le_sid;
-    unique_identity_data_t identity_data;
-    event_bitmap_t event_bitmap;
-    uint16_t le_trainer_name[8];
-    uint8_t unk4[3];
-    uint8_t flags;
-    uint8_t protocol_ver;
-    uint8_t unk5;
-    uint8_t protocol_subver;
-    uint8_t unk8;
-    uint32_t be_last_sync;
-    uint32_t be_step_count;
+    /* +0x00 */ uint32_t le_unk0;
+    /* +0x04 */ uint32_t le_unk1;
+    /* +0x08 */ uint16_t le_unk2;
+    /* +0x0a */ uint16_t le_unk3;
+    /* +0x0c */ uint16_t le_tid;
+    /* +0x0e */ uint16_t le_sid;
+    /* +0x10 */ unique_identity_data_t identity_data;
+    /* +0x38 */ event_bitmap_t event_bitmap;
+    /* +0x48 */ uint16_t le_trainer_name[8];
+    /* +0x58 */ uint8_t unk4[3];
+    /* +0x5b */ uint8_t flags;
+    /* +0x5c */ uint8_t protocol_ver;
+    /* +0x5d */ uint8_t unk5;
+    /* +0x5e */ uint8_t protocol_subver;
+    /* +0x5f */ uint8_t unk8;
+    /* +0x60 */ uint32_t be_last_sync;
+    /* +0x64 */ uint32_t be_step_count;
 } walker_info_t;
 
-typedef struct  {
-    uint32_t be_total_steps;
-    uint32_t be_today_steps;
-    uint32_t be_last_sync;
-    uint16_t be_total_days;
-    uint16_t be_current_watts;
-    uint8_t unk[4];
-    uint8_t padding[3];
-    uint8_t settings;
+/*
+ *  size: 0x18 = 24 bytes
+ *  dmitry: struct HealthData
+ */
+typedef struct {    // strut HealthData
+    /* +0x00 */ uint32_t be_total_steps;
+    /* +0x04 */ uint32_t be_today_steps;
+    /* +0x08 */ uint32_t be_last_sync;
+    /* +0x0c */ uint16_t be_total_days;
+    /* +0x0e */ uint16_t be_current_watts;
+    /* +0x10 */ uint8_t unk[4];
+    /* +0x14 */ uint8_t padding[3];
+    /* +0x17 */ uint8_t settings;
 } health_data_t;
 
 typedef struct {
@@ -88,71 +96,88 @@ typedef struct {
     uint8_t commands[0x3f];
 } lcd_config_t;
 
-typedef struct {
-    uint32_t le_current_steps;
-    uint16_t le_current_watts;
-    uint8_t padding[2];
-    uint32_t le_unk0;
-    uint16_t le_unk2;
-    uint16_t le_species;
-    uint16_t pokemon_name[11];
-    uint16_t trainer_name[8];
-    uint8_t pokemon_flags_1;
-    uint8_t pokemon_flags_2;
+/*
+ *  size: 0x38 = 56 bytes
+ *  dmitry: struct PeerPlayData
+ */
+typedef struct {    // Dmitry struct PeerPlayData
+    /* +0x00 */ uint32_t le_current_steps;
+    /* +0x04 */ uint16_t le_current_watts;
+    /* +0x06 */ uint8_t padding[2];
+    /* +0x08 */ uint32_t le_unk0;
+    /* +0x0c */ uint16_t le_unk2;
+    /* +0x0e */ uint16_t le_species;
+    /* +0x10 */ uint16_t pokemon_name[11];
+    /* +0x26 */ uint16_t trainer_name[8];
+    /* +0x36 */ uint8_t pokemon_flags_1;
+    /* +0x37 */ uint8_t pokemon_flags_2;
 } peer_play_data_t;
 
+/*
+ *  size: 16 bytes
+ *  dmitry: struct PokemonSummary
+ */
 typedef struct {
-    uint16_t le_species;
-    uint16_t le_held_item;
-    uint16_t le_moves[4];
-    uint8_t level;
-    uint8_t pokemon_flags_1;
-    uint8_t pokemon_flags_2;
-    uint8_t padding;
+    /* +0x00 */ uint16_t le_species;
+    /* +0x02 */ uint16_t le_held_item;
+    /* +0x04 */ uint16_t le_moves[4];
+    /* +0x0c */ uint8_t level;
+    /* +0x0d */ uint8_t pokemon_flags_1;    // [0..5] = variant (spinda, arceus, unown, etc.) [6] = female
+    /* +0x0e */ uint8_t pokemon_flags_2;    // [0] = has form, [1] = shiny
+    /* +0x0f */ uint8_t padding;
 } pokemon_summary_t;
 
+/*
+ *  size: 0x38 = 56 bytes
+ *  dmitry: struct TeamPokeData
+ */
 typedef struct {
-    uint16_t le_species;
-    uint16_t le_held_item;
-    uint16_t le_moves[4];
-    uint16_t le_ot_tid;
-    uint16_t le_ot_sid;
-    uint32_t le_pid;
-    uint32_t ivs;   // packed to 5-bits each
-    uint8_t evs[6];
-    uint8_t pokemon_flags_1;
-    uint8_t source_game;
-    uint8_t ability;
-    uint8_t happiness;
-    uint8_t level;
-    uint8_t padding;
-    uint16_t nickname[10];
+    /* +0x00 */ uint16_t le_species;
+    /* +0x02 */ uint16_t le_held_item;
+    /* +0x04 */ uint16_t le_moves[4];
+    /* +0x0c */ uint16_t le_ot_tid;
+    /* +0x0e */ uint16_t le_ot_sid;
+    /* +0x10 */ uint32_t le_pid;
+    /* +0x14 */ uint32_t ivs;   // packed to 5-bits each
+    /* +0x18 */ uint8_t evs[6];
+    /* +0x1e */ uint8_t pokemon_flags_1;
+    /* +0x1f */ uint8_t source_game;
+    /* +0x20 */ uint8_t ability;
+    /* +0x21 */ uint8_t happiness;
+    /* +0x22 */ uint8_t level;
+    /* +0x23 */ uint8_t padding;
+    /* +0x24 */ uint16_t nickname[10];
 } pokemon_info_t;
 
+/*
+ *  size: 0x2c = 44 bytes
+ *  dmitry: struct EventPokeExtraData
+ */
 typedef struct {
-    uint32_t le_unk0;
-    uint16_t le_ot_tid;
-    uint16_t le_ot_sid;
-    uint16_t le_unk1;
-    uint16_t le_location_met;
-    uint16_t le_unk2;
-    uint16_t ot_name[8];
-    uint8_t encounter_type;
-    uint8_t ability;
-    uint16_t le_pokeball_item;
-    uint8_t unk3[10];
+    /* +0x00 */ uint32_t le_unk0;
+    /* +0x04 */ uint16_t le_ot_tid;
+    /* +0x06 */ uint16_t le_ot_sid;
+    /* +0x08 */ uint16_t le_unk1;
+    /* +0x0a */ uint16_t le_location_met;
+    /* +0x0c */ uint16_t le_unk2;
+    /* +0x0e */ uint16_t ot_name[8];
+    /* +0x1e */ uint8_t encounter_type;
+    /* +0x1f */ uint8_t ability;
+    /* +0x20 */ uint16_t le_pokeball_item;
+    /* +0x22 */ uint8_t unk3[10];
 } special_pokemon_info_t;
 
-typedef struct {
-    uint8_t factory_data[3];
+typedef struct __attribute__((packed)) {
+    uint8_t factory_data[2];
+    uint8_t factory_data_checksum;
     unique_identity_data_t unique_data;
-    uint8_t padding_1;
+    uint8_t unique_data_checksum;
     lcd_config_t lcd_config;
-    uint8_t padding_2; // random 0xbf byte?
+    uint8_t lcd_config_checksum; // random 0xbf byte?
     walker_info_t walker_info;
-    uint8_t padding_3;
+    uint8_t walker_info_checksum;
     health_data_t health_data;
-    uint8_t padding_4;
+    uint8_t health_data_checksum;
     uint8_t copy_marker;
     uint8_t padding[16];
 } reliable_data_t;
@@ -169,48 +194,57 @@ typedef struct {
 } special_inventory_t;
 
 
+/*
+ *  size: 0xbe = 190 bytes
+ *  dmitry: struct RouteInfo
+ */
 typedef struct {
-    pokemon_summary_t pokemon_summary;
-    uint16_t pokemon_nickname[11];
-    uint8_t pokemon_happiness;
-    uint8_t route_image_index;
-    uint16_t route_name[21];
-    pokemon_summary_t route_pokemon[3];
-    uint16_t le_route_pokemon_steps[3];
-    uint8_t route_pokemon_percent[3];
-    uint8_t padding;
-    uint16_t le_route_items[10];
-    uint16_t le_route_item_steps[10];
-    uint8_t route_item_percent[10];
+    /* +0x00 */ pokemon_summary_t pokemon_summary;
+    /* +0x10 */ uint16_t pokemon_nickname[11];
+    /* +0x26 */ uint8_t pokemon_happiness;
+    /* +0x27 */ uint8_t route_image_index;
+    /* +0x28 */ uint16_t route_name[21];
+    /* +0x52 */ pokemon_summary_t route_pokemon[3];
+    /* +0x82 */ uint16_t le_route_pokemon_steps[3];
+    /* +0x88 */ uint8_t route_pokemon_percent[3];
+    /* +0x8b */ uint8_t padding;
+    /* +0x8c */ uint16_t le_route_items[10];
+    /* +0xa0 */ uint16_t le_route_item_steps[10];
+    /* +0xb4 */ uint8_t route_item_percent[10];
 } route_info_t;
 
 
+/*
+ *  size: 0x6ac = 1708 bytes
+ *  dmitry: struct SpecialRoute
+ */
 typedef struct {
-    uint8_t item_info[6];
-    uint8_t route_image_index;
-    uint8_t padding_1;
-    pokemon_summary_t special_pokemon;
-    special_pokemon_info_t special_pokemon_extra;
-    uint16_t le_special_pokemon_steps;
-    uint8_t special_pokemon_percent;
-    uint8_t padding_2;
-    uint16_t le_special_item;
-    uint16_t le_special_item_steps;
-    uint8_t special_item_percent;
-    uint8_t padding_3[3];
-    uint16_t route_name[21];
-    uint8_t pokemon_event_number;
-    uint8_t item_event_number;
-    uint8_t special_pokemon_sprite_data[0x170]; // should be 0x180 bytes, truncated
-    uint8_t special_pokemon_name_image[0x140];
-    uint8_t special_area_icon[0xc0];
-    uint8_t special_area_name_image[0x140];
-    uint8_t special_item_name_image[0x180];
+    /* +0x0000 */ uint8_t item_info[6];
+    /* +0x0006 */ uint8_t route_image_index;
+    /* +0x0007 */ uint8_t padding_1;
+    /* +0x0008 */ pokemon_summary_t special_pokemon;
+    /* +0x0018 */ special_pokemon_info_t special_pokemon_extra;
+    /* +0x0044 */ uint16_t le_special_pokemon_steps;
+    /* +0x0046 */ uint8_t special_pokemon_percent;
+    /* +0x0047 */ uint8_t padding_2;
+    /* +0x0048 */ uint16_t le_special_item;
+    /* +0x004a */ uint16_t le_special_item_steps;
+    /* +0x004c */ uint8_t special_item_percent;
+    /* +0x004d */ uint8_t padding_3[3];
+    /* +0x0050 */ uint16_t route_name[21];
+    /* +0x007a */ uint8_t pokemon_event_number;
+    /* +0x007b */ uint8_t item_event_number;
+    /* +0x007c */ uint8_t special_pokemon_sprite_data[0x170]; // should be 0x180 bytes, truncated
+    /* +0x01ec */ uint8_t special_pokemon_name_image[0x140];
+    /* +0x032c */ uint8_t special_area_icon[0xc0];
+    /* +0x03ec */ uint8_t special_area_name_image[0x140];
+    /* +0x052c */ uint8_t special_item_name_image[0x180];
 } special_route_info_t;
 
 /*
- *  size: 0x5e = 94 bytes
+ *  size: 0x88 = 136 bytes
  *  dmitry: struct EventLogItem
+ *  Modified from dmitry's version
  */
 typedef struct {
     /* 0x00 */ uint32_t be_time;
@@ -222,61 +256,62 @@ typedef struct {
     /* 0x10 */ uint16_t other_trainer_name[8];
     /* 0x20 */ uint16_t our_pokemon_name[11];
     /* 0x36 */ uint16_t other_pokemon_name[11];
-    /* 0x4c */ uint8_t  route_image_index;
-    /* 0x4d */ uint8_t  pokemon_friendship;
-    /* 0x4e */ uint16_t be_our_watts;
-    /* 0x50 */ uint16_t be_other_watts;
-    /* 0x52 */ uint32_t be_steps;
-    /* 0x56 */ uint32_t be_other_steps;
-    /* 0x5a */ uint8_t  event_type;
-    /* 0x5b */ uint8_t  our_pokemon_flags;
-    /* 0x5c */ uint8_t  other_pokemon_flags;
-    /* 0x5d */ uint8_t  padding;
+    /* 0x4c */ uint16_t route_name[21];
+    /* 0x76 */ uint8_t  route_image_index;
+    /* 0x77 */ uint8_t  pokemon_friendship;
+    /* 0x78 */ uint16_t be_our_watts;
+    /* 0x7a */ uint16_t be_other_watts;
+    /* 0x7c */ uint32_t be_steps;
+    /* 0x80 */ uint32_t be_other_steps;
+    /* 0x84 */ uint8_t  event_type;
+    /* 0x85 */ uint8_t  our_pokemon_flags;
+    /* 0x86 */ uint8_t  other_pokemon_flags;
+    /* 0x87 */ uint8_t  padding;
 } event_log_item_t;
 
 
 
 typedef enum {
-    EVENT_TYPE_EMPTY_ENTRY,
-    EVENT_TYPE_PEER_PLAY_1,
-    EVENT_TYPE_PEER_PLAY_2,
-    EVENT_TYPE_PEER_PLAY_3,
-    EVENT_TYPE_PEER_PLAY_4,
-    EVENT_TYPE_PEER_PLAY_5,
-    EVENT_TYPE_PEER_PLAY_6,
-    EVENT_TYPE_PEER_PLAY_7,
-    EVENT_TYPE_PEER_PLAY_8,
-    EVENT_TYPE_PEER_PLAY_9,
-    EVENT_TYPE_PEER_PLAY_10,
-    EVENT_TYPE_ITEM_DOWSED,
-    EVENT_TYPE_SPECIAL_ITEM_DOWSED,     //special event item
-    EVENT_TYPE_POKEMON_CAUGHT,
-    EVENT_TYPE_SPECIAL_POKEMON_CAUGHT,  //special event poke
-    EVENT_TYPE_POKEMON_RAN,			    //0x0f
-    EVENT_TYPE_POKEMON_LOST,            //lost to radared poke 0x10
-    EVENT_TYPE_POKEMON_FOUND_ITEM,      //idle poke finds item 0x11
-    EVENT_TYPE_MOOD_HAPPY,
-    EVENT_TYPE_MOOD_RUNNING,
-    EVENT_TYPE_MOOD_LOOKING_AWAY,
-    EVENT_TYPE_MOOD_BORED,
-    EVENT_TYPE_MOOD_GO_HOME,
-    EVENT_TYPE_POKEMON_JOINED,
-    EVENT_TYPE_WALK_ENDED,
-    EVENT_TYPE_WALK_STARTED,
-    EVENT_TYPE_PLAYED_ALOT,
-    EVENT_TYPE_FELL_ASLEEP,
-    EVENT_TYPE_ITEM_GIFTED,
+    /* 0x00 */ EVENT_TYPE_EMPTY_ENTRY,
+    /* 0x01 */ EVENT_TYPE_PEER_PLAY_1,
+    /* 0x02 */ EVENT_TYPE_PEER_PLAY_2,
+    /* 0x03 */ EVENT_TYPE_PEER_PLAY_3,
+    /* 0x04 */ EVENT_TYPE_PEER_PLAY_4,
+    /* 0x05 */ EVENT_TYPE_PEER_PLAY_5,
+    /* 0x06 */ EVENT_TYPE_PEER_PLAY_6,
+    /* 0x07 */ EVENT_TYPE_PEER_PLAY_7,
+    /* 0x08 */ EVENT_TYPE_PEER_PLAY_8,
+    /* 0x09 */ EVENT_TYPE_PEER_PLAY_9,
+    /* 0x0a */ EVENT_TYPE_PEER_PLAY_10,
+    /* 0x0b */ EVENT_TYPE_ITEM_DOWSED,
+    /* 0x0c */ EVENT_TYPE_SPECIAL_ITEM_DOWSED,
+    /* 0x0d */ EVENT_TYPE_POKEMON_CAUGHT,
+    /* 0x0e */ EVENT_TYPE_SPECIAL_POKEMON_CAUGHT,
+    /* 0x0f */ EVENT_TYPE_POKEMON_RAN,
+    /* 0x10 */ EVENT_TYPE_POKEMON_LOST,
+    /* 0x11 */ EVENT_TYPE_POKEMON_FOUND_ITEM,
+    /* 0x12 */ EVENT_TYPE_MOOD_HAPPY,
+    /* 0x13 */ EVENT_TYPE_MOOD_RUNNING,
+    /* 0x14 */ EVENT_TYPE_MOOD_LOOKING_AWAY,
+    /* 0x15 */ EVENT_TYPE_MOOD_BORED,
+    /* 0x16 */ EVENT_TYPE_MOOD_GO_HOME,
+    /* 0x17 */ EVENT_TYPE_POKEMON_JOINED,
+    /* 0x18 */ EVENT_TYPE_WALK_ENDED,
+    /* 0x19 */ EVENT_TYPE_WALK_STARTED,
+    /* 0x1a */ EVENT_TYPE_PLAYED_ALOT,
+    /* 0x1b */ EVENT_TYPE_FELL_ASLEEP,
+    /* 0x1c */ EVENT_TYPE_ITEM_GIFTED,
 } event_log_type_t;
 
 typedef enum {
-    ROUTE_IMAGE_FIELD_AND_TREES,
-    ROUTE_IMAGE_FOREST_AND_TREES,
-    ROUTE_IMAGE_SUBURBS,
-    ROUTE_IMAGE_URBAN,
-    ROUTE_IMAGE_MOUNTAIN,
-    ROUTE_IMAGE_CAVE,
-    ROUTE_IMAGE_LAKE,
-    ROUTE_IMAGE_BEACH,
+    /* 0x00 */ ROUTE_IMAGE_FIELD_AND_TREES,
+    /* 0x01 */ ROUTE_IMAGE_FOREST_AND_TREES,
+    /* 0x02 */ ROUTE_IMAGE_SUBURBS,
+    /* 0x03 */ ROUTE_IMAGE_URBAN,
+    /* 0x04 */ ROUTE_IMAGE_MOUNTAIN,
+    /* 0x05 */ ROUTE_IMAGE_CAVE,
+    /* 0x06 */ ROUTE_IMAGE_LAKE,
+    /* 0x07 */ ROUTE_IMAGE_BEACH,
 } route_image_index_t;
 
 
