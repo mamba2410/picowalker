13bceae22abe4349fa9d0975567134273ce7ce40
mamba2410 <callummcgregor54@gmail.com>
2024-01-10 20:56:20 +0000

    src: change timer code for updates pico-sdk api

--- src/drivers/timer_pico.c
+++ src/drivers/timer_pico.c
@@ -1,10 +1,14 @@
 #include <stdint.h>
 
 #include "pico/time.h"
+#include "pico/types.h"
 
 uint64_t pw_now_us() {
     absolute_time_t now = get_absolute_time();
-    return now._private_us_since_boot;
+    // this is mega hacky, but I don't know how else to get an
+    // absolute time stamp in us as a u64
+    //return now._private_us_since_boot;
+    return (uint64_t)now;
 }
 
 void pw_timer_delay_ms(uint64_t ms) {
