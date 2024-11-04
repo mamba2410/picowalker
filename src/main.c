06fc97afa8cd88236584077608a8376cbae9cbe5
mamba2410 <callummcgregor54@gmail.com>
2024-11-04 22:08:37 +0000

    debug: add way to program the eeprom over uart

--- src/main.c
+++ src/main.c
@@ -20,6 +20,26 @@ int main() {
 
     printf("Hello, picowalker!\n");
 
+    /*
+    // Program the EEPROM over debug uart
+    // Send a full rom file using `cat`
+    // Beware: no validation is done
+    pw_eeprom_init();
+
+    uint16_t address = 0;
+    uint8_t page[128];
+    while(1){
+        if(uart_is_readable(uart0)) {
+            page[address&0x7f] = uart_getc(uart0);
+            address++;
+            if((address&0x7f) == 0) {
+                pw_eeprom_write(address-128, page, 128);
+                printf("Written page to 0x%04x\n", address-128);
+            }
+        }
+    }
+    */
+
     // Start picowalker-core
     walker_entry();
 
