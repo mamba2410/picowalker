404c779653021f17825138b23a8b0a6c54d47847
mamba2410 <callummcgregor54@gmail.com>
2024-11-05 21:29:53 +0000

    battery: add basic i2c, maybe works

--- src/drivers/battery_pico_bq25628e.c
+++ src/drivers/battery_pico_bq25628e.c
@@ -8,9 +8,25 @@
 #include "battery_pico_bq25628e.h"
 #include "../picowalker-defs.h"
 
+void pw_battery_int() {
+
+}
+
 void pw_battery_init() {
-    // TODO: start i2c
-    // TODO: any init things
+
+    i2c_init(PMIC_I2C, PMIC_I2C_SPEED_KHZ*1000);
+    gpio_set_function(PMIC_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PMIC_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    // hardware pull-ups
+
+    // init things
+    uint8_t buf[4] = {0};
+
+    // Check for part number
+    i2c_write_blocking(PMIC_I2C, PMIC_I2C_ADDRESS, buf, 1, false);
+    i2c_read_blocking(PMIC_I2C, PMIC_I2C_ADDRESS, buf, 1, false);
+
+    printf("BQ25628E part info: 0x%02x\n", buf[0]);
 }
 
 pw_battery_status_t pw_battery_get_status() {
--- src/drivers/battery_pico_bq25628e.h
+++ src/drivers/battery_pico_bq25628e.h
@@ -8,8 +8,17 @@
 #define BAT_INT_PIN 27  // GP27, phys 32
 #define BAT_CE_PIN  28  // GP28, phys 34
 
-#define I2C_SCL_PIN 21  // GP21, phys 27
-#define I2C_SDA_PIN 20  // GP20, phys 26
+#define PMIC_I2C_SCL_PIN 21  // GP21, phys 27
+#define PMIC_I2C_SDA_PIN 20  // GP20, phys 26
+
+#define PMIC_I2C_SPEED_KHZ 100 // Supports 400 and 1000 also but needs more timing requirements
+#define PMIC_I2C_ADDRESS 0x6a
+
+#define PMIC_I2C i2c0
+
+enum bq25628e_reg_address_e {
+    REG0x38_PART_INFO = 0x38,
+};
 
 #endif /* BATTERY_PICO_B25628E_H */
 
