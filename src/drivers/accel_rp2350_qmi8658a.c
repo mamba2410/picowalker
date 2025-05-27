4f4e5f27342c9565eb6c248b203dda86f42f041b
Dave uRrr <47121419+DaveuRrr@users.noreply.github.com>
2025-05-27 00:08:37 -0700

    RP2350 Updates
    It should be backwards compatible but I am changing the code for RP2350 Touch LCD 1.28in

---
+++ src/drivers/accel_rp2350_qmi8658a.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+
+#include "accel_rp2350_qmi8658a.h"
+#include "gpio_interrupts_pico.h"
