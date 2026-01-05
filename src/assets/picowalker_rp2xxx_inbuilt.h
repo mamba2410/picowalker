49a2de6364d684c01ff61f1c7524f9d7051e92ca
mamba2410 <callummcgregor54@gmail.com>
2026-01-05 21:40:53 +0000

    assets: add some custom picowalker assets

--- src/assets/picowalker_rp2xxx_inbuilt.h
+++ src/assets/picowalker_rp2xxx_inbuilt.h
@@ -1,22 +1,15 @@
 #ifndef PWROMS_H
 #define PWROMS_H
 
-
 #define FLASHROM_SIZE	48*1024
 #define EEPROM_SIZE		64*1024
-
-#define SAD_POKEWALKER_SIZE 576
 
 #ifndef __ASSEMBLER__
 
 #include <stdint.h>
 
-/*
- *  These are stored in ram (for now)
- */
-//extern uint8_t eeprom[EEPROM_SIZE];
-//extern uint8_t flashrom[FLASHROM_SIZE];
-extern uint8_t sad_pokewalker[SAD_POKEWALKER_SIZE];
+extern uint8_t *batterY_fancy_text;
+extern size_t battery_fancy_text_len;
 
 #endif /* __ASSEMBLER__ */
 
