868ff3352f0f889914523fd8275ddd1c8e7142ba
mamba2410 <mambawastaken@proton.me>
2026-08-30 16:16:34 +0100

    lsp: format interrupt files

--- src/drivers/interrupts/rp2xxx_gpio.c
+++ src/drivers/interrupts/rp2xxx_gpio.c
@@ -1,39 +1,39 @@
-#include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#include "board_resources.h"
-#include "../input/buttons_rp2xxx_gpio.h"
-#include "../battery/bq25628e_rp2xxx_i2c.h"
-#include "../accel/bma400_rp2xxx_spi.h"
-#include "../sleep/dormant_rp2xxx.h"
 #include "../../picowalker_structures.h"
+#include "../accel/bma400_rp2xxx_spi.h"
+#include "../battery/bq25628e_rp2xxx_i2c.h"
+#include "../input/buttons_rp2xxx_gpio.h"
+#include "../sleep/dormant_rp2xxx.h"
+#include "board_resources.h"
 
 void pw_gpio_interrupt_handler(uint gp, uint32_t events) {
-    //printf("[Debug] interrupt on pin GP%d\n", gp);
-    switch(gp) {
-    case BUTTON_LEFT_PIN:
-    case BUTTON_MIDDLE_PIN:
-    case BUTTON_RIGHT_PIN: {
-        wake_reason |= PW_WAKE_REASON_BUTTON;
-        pw_pico_button_callback(gp, events);
-        break;
-    }
-    case BAT_INT_PIN: {
-        wake_reason |= PW_WAKE_REASON_BATTERY;
-        bq25628e_irq_minimal();
-        break;
-    }
-    case ACCEL_INT_PIN: {
-        //printf("[Debug] Accel interrupt\n");
-        //pw_accel_reset_int();
-        wake_reason |= PW_WAKE_REASON_ACCEL;
-        break;
-    }
-    default: {
-        printf("[Warn] Unhandled interrupt on pin GP%d\n", gp);
-        break;
-    }
+    // printf("[Debug] interrupt on pin GP%d\n", gp);
+    switch (gp) {
+        case BUTTON_LEFT_PIN:
+        case BUTTON_MIDDLE_PIN:
+        case BUTTON_RIGHT_PIN: {
+            wake_reason |= PW_WAKE_REASON_BUTTON;
+            pw_pico_button_callback(gp, events);
+            break;
+        }
+        case BAT_INT_PIN: {
+            wake_reason |= PW_WAKE_REASON_BATTERY;
+            bq25628e_irq_minimal();
+            break;
+        }
+        case ACCEL_INT_PIN: {
+            // printf("[Debug] Accel interrupt\n");
+            // pw_accel_reset_int();
+            wake_reason |= PW_WAKE_REASON_ACCEL;
+            break;
+        }
+        default: {
+            printf("[Warn] Unhandled interrupt on pin GP%d\n", gp);
+            break;
+        }
     }
 }
--- src/drivers/interrupts/rp2xxx_gpio.h
+++ src/drivers/interrupts/rp2xxx_gpio.h
@@ -1,11 +1,10 @@
 #ifndef PW_INTERRUPTS_PICO_H
 #define PW_INTERRUPTS_PICO_H
 
-#include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 void pw_gpio_interrupt_handler(uint gp, uint32_t events);
 
 #endif /* PW_INTERRUPTS_PICO_H */
-
